1fc41b01470b303927a42c951d21f11aaad88fd3
hoangngocbaominh <hoangngocbaominh1998@gmail.com>
2021-07-14 16:02:17 +0700

    upload

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.static("public"));
 app.use(cors({
   origin: "*"
 }))
-app.get("/", (req, res) => res.render("index"));
+// app.get("/", (req, res) => res.render("index"));
 
 app.post("/upload", (req, res) => {
   upload(req, res, (err) => {
